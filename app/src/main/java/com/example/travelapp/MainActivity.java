54c43045b8cd90c4ec7ab7efc2e806f4037c1e4e
immonica <imonicaanamaria@gmail.com>
2024-01-23 19:09:38 +0200

    Google Maps Implementation + Timisoara localization

--- app/src/main/java/com/example/travelapp/MainActivity.java
+++ app/src/main/java/com/example/travelapp/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.travelapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.internal.ICameraUpdateFactoryDelegate;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap myMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        myMap = googleMap;
+
+        LatLng timisoara = new LatLng(45.7494, 21.2272);
+        myMap.addMarker(new MarkerOptions().position(timisoara).title("Timisoara"));
+        myMap.moveCamera(CameraUpdateFactory.newLatLng(timisoara));
     }
 }
