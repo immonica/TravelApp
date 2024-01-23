8defaa01b8210ac0aa9908d78487cf630e70b182
immonica <imonicaanamaria@gmail.com>
2024-01-23 20:47:50 +0200

    Google Maps Implementation + Current Location

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,5 +39,5 @@ dependencies {
 
     // Maps SDK for Android
     implementation("com.google.android.gms:play-services-maps:18.2.0")
-
+    implementation("com.google.android.gms:play-services-location:21.1.0")
 }
