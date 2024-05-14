b5faae7d8ebde1515f984262fb892dd040507619
immonica <imonicaanamaria@gmail.com>
2024-05-14 23:16:20 +0300

    Logout functionality with confirmation dialog

--- app/src/main/java/fragments/PopUpFragment.java
+++ app/src/main/java/fragments/PopUpFragment.java
@@ -4,10 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.google.firebase.auth.FirebaseAuth;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
 
 import com.example.travelapp.R;
 
@@ -31,12 +36,51 @@ public class PopUpFragment extends Fragment {
             }
         });
 
+        // Find the logout_button
+        Button logoutButton = rootView.findViewById(R.id.logout_button);
+
+        // Set OnClickListener for the logout_button
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Call the logout method
+                logoutUser();
+            }
+        });
+
         return rootView;
     }
 
     private void closePopUpFragment() {
         // Go back to the previous fragment (HomeFragment)
         requireActivity().getSupportFragmentManager().popBackStack();
+    }
+
+    private void logoutUser() {
+        // Build and show a confirmation dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setTitle("Logout");
+        builder.setMessage("Are you sure you want to logout?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // User clicked Yes, proceed with logout
+                FirebaseAuth.getInstance().signOut();
+                // Navigate to LoginFragment after logout
+                getParentFragmentManager().beginTransaction()
+                        .replace(R.id.fragmentContainer, new LoginFragment())
+                        .commit();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // User clicked No, dismiss the dialog
+                dialog.dismiss();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
 }
